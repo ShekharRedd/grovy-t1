9ff3390bf5019960da263b5bab56142e6d863b95
ShekharRedd <shekharreddy4558@gmail.com>
2023-05-14 16:02:50 +0530

    intial

--- script.groovy
+++ script.groovy
@@ -3,7 +3,7 @@ def build(){
 }
 def test(){
     echo "testing the application"
-    withCredentials([usernamePassword(credentialsId:'0672b53e-cc7a-46c7-aad8-bf5e6a6d1325', usernameVariable: 'USERNAME',passwordVariable:'PASSWORD')])
+    withCredentials([usernamePassword(credentialsId:'Docker-hub', usernameVariable: 'USERNAME',passwordVariable:'PASSWORD')])
     sh 'docker build -t shekhar123reddy/num:34.5'
     sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
     sh "docker push shekhar123reddy/num:34.5"
